a673d258a79c0aa444c8a4637f0011a92c822c44
JieWang <qdndwj@gmail.com>
2021-02-18 17:24:22 +0800

    JasperReports update

--- health_backend/src/main/java/com/Jaziel/controller/ReportController.java
+++ health_backend/src/main/java/com/Jaziel/controller/ReportController.java
@@ -6,12 +6,19 @@ import com.Jaziel.service.MemberService;
 import com.Jaziel.service.ReportService;
 import com.Jaziel.service.SetmealService;
 import com.alibaba.dubbo.config.annotation.Reference;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
@@ -146,7 +153,7 @@ public class ReportController {
             }
 
             //使用输出流进行表格下载,基于浏览器作为客户端下载
-            OutputStream out = response.getOutputStream();
+            ServletOutputStream out = response.getOutputStream();
             response.setContentType("application/vnd.ms-excel");//代表的是Excel文件类型
             response.setHeader("content-Disposition", "attachment;filename=report.xlsx");//指定以附件形式进行下载
             workbook.write(out);
@@ -154,6 +161,37 @@ public class ReportController {
             out.flush();
             out.close();
             workbook.close();
+            return null;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @RequestMapping("/exportBusinessReportPDF")
+    public Result exportBusinessReportPDF(HttpServletRequest request, HttpServletResponse response){
+        try {
+            //取出返回结果数据，准备将报表数据写入到PDF文件中
+            Map<String, Object> result = reportService.getBusinessReportData();
+            List<Map> hotSetmeal = (List<Map>) result.get("hotSetmeal");
+
+            //动态获取模板文件绝对磁盘路径
+            String jrxmlPath = request.getSession().getServletContext().getRealPath("template") + File.separator + "health_business3.jrxml";
+            String jasperPath = request.getSession().getServletContext().getRealPath("template") + File.separator + "health_business3.jasper";
+            //编译模板
+            JasperCompileManager.compileReportToFile(jrxmlPath, jasperPath);
+            //填充数据---使用JavaBean数据源方式填充
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperPath, result, new JRBeanCollectionDataSource(hotSetmeal));
+
+            //使用输出流进行表格下载,基于浏览器作为客户端下载
+            ServletOutputStream out = response.getOutputStream();
+            response.setContentType("application/pdf");//代表的是Excel文件类型
+            response.setHeader("content-Disposition", "attachment;filename=report.pdf");//指定以附件形式进行下载
+
+            //输出文件
+            JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+            out.flush();
+            out.close();
             return null;
         }catch (Exception e){
             e.printStackTrace();
