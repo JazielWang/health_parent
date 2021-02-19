e8b39c1e96bdf3406e55fd2eb1714e1a0960e082
JieWang <qdndwj@gmail.com>
2021-02-19 23:31:43 +0800

    redis update

--- health_backend/src/main/java/com/Jaziel/controller/ReportController.java
+++ health_backend/src/main/java/com/Jaziel/controller/ReportController.java
@@ -44,26 +44,26 @@ public class ReportController {
     private ReportService reportService;
 
     @RequestMapping("/getMemberReport")
-    public Result getMemberReport(){
+    public Result getMemberReport() {
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.MONTH, -12);
         Map<String, Object> map = new HashMap<>();
         List<String> months = new ArrayList<>();
         List<Integer> memberCount = new ArrayList<>();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM");
-        for (int i = 0; i < 12; i++){
+        for (int i = 0; i < 12; i++) {
             calendar.add(Calendar.MONTH, 1);
             months.add(simpleDateFormat.format(calendar.getTime()));
         }
         memberCount = memberService.countMemberByMonths(months);
-        map.put("months",months);
+        map.put("months", months);
         map.put("memberCount", memberCount);
         return new Result(true, MessageConstant.GET_MEMBER_NUMBER_REPORT_SUCCESS, map);
     }
 
     @RequestMapping("/getSetmealReport")
-    public Result getSetmealReport(){
-        try{
+    public Result getSetmealReport() {
+        try {
             // 获得setmeal统计数据
             List<Map<String, Object>> setmealCount = setmealService.getSetmealReport();
             Map<String, Object> data = new HashMap<>();
@@ -76,18 +76,18 @@ public class ReportController {
             }
             data.put("setmealNames", setmealNames);
             return new Result(true, MessageConstant.GET_SETMEAL_COUNT_REPORT_SUCCESS, data);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return new Result(false, MessageConstant.GET_SETMEAL_COUNT_REPORT_FAIL);
         }
     }
 
     @RequestMapping("/getBusinessReportData")
-    public Result getBusinessReportData(){
+    public Result getBusinessReportData() {
         try {
             Map<String, Object> data = reportService.getBusinessReportData();
             return new Result(true, MessageConstant.GET_MEMBER_NUMBER_REPORT_SUCCESS, data);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return new Result(false, MessageConstant.GET_MEMBER_NUMBER_REPORT_FAIL);
         }
@@ -95,7 +95,7 @@ public class ReportController {
     }
 
     @RequestMapping("/exportBusinessReport")
-    public Result exportBusinessReport(HttpServletRequest request, HttpServletResponse response){
+    public Result exportBusinessReport(HttpServletRequest request, HttpServletResponse response) {
         try {
             Map<String, Object> result = reportService.getBusinessReportData();
 
@@ -142,11 +142,11 @@ public class ReportController {
             row.getCell(7).setCellValue(thisMonthVisitsNumber);//本月到诊数
 
             int rowNum = 12;
-            for(Map map : hotSetmeal){//热门套餐
+            for (Map map : hotSetmeal) {//热门套餐
                 String name = (String) map.get("name");
                 Long setmeal_count = (Long) map.get("setmeal_count");
                 BigDecimal proportion = (BigDecimal) map.get("proportion");
-                row = sheet.getRow(rowNum ++);
+                row = sheet.getRow(rowNum++);
                 row.getCell(4).setCellValue(name);//套餐名称
                 row.getCell(5).setCellValue(setmeal_count);//预约数量
                 row.getCell(6).setCellValue(proportion.doubleValue());//占比
@@ -155,21 +155,21 @@ public class ReportController {
             //使用输出流进行表格下载,基于浏览器作为客户端下载
             ServletOutputStream out = response.getOutputStream();
             response.setContentType("application/vnd.ms-excel");//代表的是Excel文件类型
-            response.setHeader("content-Disposition", "attachment;filename=report.xlsx");//指定以附件形式进行下载
+            response.setHeader("Content-Disposition", "attachment;filename=report.xlsx");//指定以附件形式进行下载
             workbook.write(out);
 
             out.flush();
             out.close();
             workbook.close();
             return null;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
     @RequestMapping("/exportBusinessReportPDF")
-    public Result exportBusinessReportPDF(HttpServletRequest request, HttpServletResponse response){
+    public Result exportBusinessReportPDF(HttpServletRequest request, HttpServletResponse response) {
         try {
             //取出返回结果数据，准备将报表数据写入到PDF文件中
             Map<String, Object> result = reportService.getBusinessReportData();
@@ -186,14 +186,15 @@ public class ReportController {
             //使用输出流进行表格下载,基于浏览器作为客户端下载
             ServletOutputStream out = response.getOutputStream();
             response.setContentType("application/pdf");//代表的是Excel文件类型
-            response.setHeader("content-Disposition", "attachment;filename=report.pdf");//指定以附件形式进行下载
+            // 此处的设置表头，“Content-Disposition” 需要首字母大写
+            response.setHeader("Content-Disposition", "attachment;filename=report.pdf");//指定以附件形式进行下载
 
             //输出文件
             JasperExportManager.exportReportToPdfStream(jasperPrint, out);
             out.flush();
             out.close();
             return null;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
